abe4b7480a89e4c6cf1edcc208c07a892dd2da0e
RJ Salvador <salvador.workshop@gmail.com>
2025-06-03 23:27:50 -0400

    Markdown resume complete

--- src/resume_builder/resume_builder.py
+++ src/resume_builder/resume_builder.py
@@ -77,15 +77,56 @@ def build_resume_full_md(resume_info):
 
     out_file_full_md.write(f"## Technical Experience\n\n")
 
+    for work_exp in resume_info["work_experience"]:
+        out_file_full_md.write(
+            f"### {work_exp["exp_role"]}, {work_exp["org_name"]}\n\n"
+        )
+
+        end_date_str = "Present" if not work_exp["end_date"] else work_exp["end_date"]
+        out_file_full_md.write(f"_{work_exp["start_date"]} — {end_date_str}  \n")
+        out_file_full_md.write(f"{work_exp["org_location"]}  \n")
+        out_file_full_md.write(
+            f"Core technologies — {', '.join(work_exp["skills"])}_\n\n"
+        )
+        for highlight in work_exp["highlights"]:
+            out_file_full_md.write(f"- {highlight}\n")
+        out_file_full_md.write("\n")
+
     # ------------
     # Projects
 
     out_file_full_md.write(f"## Projects\n\n")
 
+    for proj_exp in resume_info["projects"]:
+        out_file_full_md.write(
+            f"### {proj_exp["project_name"]}, {proj_exp["org_name"]}\n\n"
+        )
+        end_date_str = "Present" if not proj_exp["end_date"] else proj_exp["end_date"]
+        out_file_full_md.write(f"_{proj_exp["start_date"]} — {end_date_str}  \n")
+        out_file_full_md.write(f"{proj_exp["org_location"]}  \n")
+        out_file_full_md.write(f"Core skills — {', '.join(proj_exp["skills"])}_\n\n")
+        for highlight in proj_exp["highlights"]:
+            out_file_full_md.write(f"- {highlight}\n")
+        out_file_full_md.write("\n")
+
     # ------------
     # Technical Education
 
     out_file_full_md.write(f"## Technical Education\n\n")
+
+    for edu_exp in resume_info["education"]:
+        out_file_full_md.write(
+            f"### {edu_exp["education_cred"]}, {edu_exp["org_name"]}\n\n"
+        )
+        end_date_str = "Present" if not edu_exp["end_date"] else edu_exp["end_date"]
+        out_file_full_md.write(f"_{edu_exp["start_date"]} — {end_date_str}  \n")
+        out_file_full_md.write(f"{edu_exp["org_location"]}  \n")
+        out_file_full_md.write(
+            f"Core technologies — {', '.join(edu_exp["skills"])}_\n\n"
+        )
+        for highlight in edu_exp["highlights"]:
+            out_file_full_md.write(f"- {highlight}\n")
+        out_file_full_md.write("\n")
 
     # ------------
     # About Me

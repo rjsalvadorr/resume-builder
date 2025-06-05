f723c516398354f97fe335e8f82381fac0dff9ec
RJ Salvador <salvador.workshop@gmail.com>
2025-06-05 17:28:17 -0400

    nice text updates

--- src/resume_builder/resume_builder_md.py
+++ src/resume_builder/resume_builder_md.py
@@ -15,7 +15,13 @@ def build_resume_full_md(resume_info):
     # ------------
     # Contact Info
 
+    contact_idx = 0
+    num_break = 3
     for contact in resume_info["contact_info"]:
+        if contact_idx == num_break:
+            out_file_full_md.write("  \n")
+        elif contact_idx != 0:
+            out_file_full_md.write(" &nbsp; &nbsp; ")
         link = contact["info"]
         copy = contact["info"].replace("https://", "")
 
@@ -27,8 +33,9 @@ def build_resume_full_md(resume_info):
             link = f"tel:{format_phone_num(contact["info"], "html")}"
             copy = format_phone_num(contact["info"])
 
-        out_file_full_md.write(f"[{copy}]({link})  \n")
-    out_file_full_md.write("\n")
+        out_file_full_md.write(f"[{copy}]({link})")
+        contact_idx = contact_idx + 1
+    out_file_full_md.write("\n\n")
 
     # ------------
     # Objective
@@ -59,8 +66,9 @@ def build_resume_full_md(resume_info):
         end_date_str = (
             "Present" if not work_exp["end_date"] else format_date(work_exp["end_date"])
         )
-        out_file_full_md.write(f"_{start_date_str} — {end_date_str}  \n")
-        out_file_full_md.write(f"{work_exp["org_location"]}  \n")
+        out_file_full_md.write(
+            f"_{start_date_str} — {end_date_str} &nbsp;in&nbsp; {work_exp["org_location"]}  \n"
+        )
         out_file_full_md.write(
             f"Core technologies — {', '.join(work_exp["skills"])}_\n\n"
         )
@@ -81,8 +89,9 @@ def build_resume_full_md(resume_info):
         end_date_str = (
             "Present" if not proj_exp["end_date"] else format_date(proj_exp["end_date"])
         )
-        out_file_full_md.write(f"_{start_date_str} — {end_date_str}  \n")
-        out_file_full_md.write(f"{proj_exp["org_location"]}  \n")
+        out_file_full_md.write(
+            f"_{start_date_str} — {end_date_str} &nbsp;in&nbsp; {proj_exp["org_location"]}  \n"
+        )
         out_file_full_md.write(f"Core skills — {', '.join(proj_exp["skills"])}_\n\n")
         for highlight in proj_exp["highlights"]:
             out_file_full_md.write(f"- {highlight}\n")
@@ -101,11 +110,10 @@ def build_resume_full_md(resume_info):
         end_date_str = (
             "Present" if not edu_exp["end_date"] else format_date(edu_exp["end_date"])
         )
-        out_file_full_md.write(f"_{start_date_str} — {end_date_str}  \n")
-        out_file_full_md.write(f"{edu_exp["org_location"]}  \n")
         out_file_full_md.write(
-            f"Core technologies — {', '.join(edu_exp["skills"])}_\n\n"
+            f"_{start_date_str} — {end_date_str} &nbsp;in&nbsp; {edu_exp["org_location"]}  \n"
         )
+        out_file_full_md.write(f"Core skills — {', '.join(edu_exp["skills"])}_\n\n")
         for highlight in edu_exp["highlights"]:
             out_file_full_md.write(f"- {highlight}\n")
         out_file_full_md.write("\n")
